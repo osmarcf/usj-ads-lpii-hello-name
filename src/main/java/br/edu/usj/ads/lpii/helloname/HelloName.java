9c3773977099a8bbc48740f6a7c2c9fe00871901
Osmar da Cunha Filho <osmarcf@gmail.com>
2020-04-14 22:13:50 -0300

    primeiro commit

---
+++ src/main/java/br/edu/usj/ads/lpii/helloname/HelloName.java
@@ -0,0 +1,23 @@
+package br.edu.usj.ads.lpii.helloname;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloName {
+
+    @PostMapping(value="helloName")
+    public ModelAndView postHelloName(@RequestParam String nome) {
+        String mensagem = "Olá, " + nome + "!";
+
+        ModelAndView modelAndView = new ModelAndView("hello-name");
+        
+        modelAndView.addObject("mensagem", mensagem);
+        
+        return modelAndView;
+    }
+    
+}
